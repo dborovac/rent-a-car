b6169a60f569d14ae21a4eb6277d06eb8be9a0be
Dario Borovac <borovacdario4@gmail.com>
2022-01-16 22:15:31 +0100

    added client-side validation

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
 function init() {
-    localStorage.setItem("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEyLCJlbWFpbCI6Im5ld0BtYWlsLm5ldCIsInJvbGUiOiJBZG1pbiIsImlhdCI6MTY0MTkyMTQ1Nn0.d3ObpVzdqQ60WG3Lw8r-SCc29s06Tq6oAKnDuxFcvEY");
+    localStorage.setItem("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEyLCJlbWFpbCI6Im5ld0BtYWlsLm5ldCIsInJvbGUiOiJBZG1pbiIsImlhdCI6MTY0MjM1NTgzNX0.uIVzjqWBirSN90iweHN4oO8Rvh2BpkgJRZV4nyawf_k");
     console.log(localStorage);
 }
